4b8f049f36fb09b58a8c4ac538842ed73b9abeaf
repath500 <75042922+repath500@users.noreply.github.com>
2024-04-28 11:15:27 +0100

    Update healthz.ts

--- src/pages/api/healthz.ts
+++ src/pages/api/healthz.ts
@@ -1,3 +1,4 @@
+ypescript
 export const config = {
   runtime: 'edge'
 }
@@ -7,21 +8,21 @@ const handler = async (req: any): Promise<Response> => {
 
   try {
     let azureOpenAIKey = false
-    let openAIKey = false
+    let groqKey = false
     if (process.env.AZURE_OPENAI_API_KEY) {
       if (process.env.AZURE_OPENAI_API_KEY.trim() !== '') {
         azureOpenAIKey = true
       }
     }
 
-    if (process.env.OPENAI_API_KEY) {
-      if (process.env.OPENAI_API_KEY.trim() !== '') {
-        openAIKey = true
+    if (process.env.GROQ_API_KEY) {
+      if (process.env.GROQ_API_KEY.trim() !== '') {
+        groqKey = true
       }
     }
 
-    if (!azureOpenAIKey && !openAIKey) {
-      return new Response('OpenAI key is empty')
+    if (!azureOpenAIKey && !groqKey) {
+      return new Response('GROQ API key is empty')
     }
     return new Response('Ok')
   } catch (error) {
